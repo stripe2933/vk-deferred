48722e132104746fde50749f7429bbab4b4bf327
gomkyung2 <stripe2933@gmail.com>
2024-07-22 02:17:35 +0900

    WIP: Use vku with vcpkg overlays.

--- .github/workflows/clang-toolchain.cmake
+++
@@ -1,2 +0,0 @@
-set(CMAKE_C_COMPILER /usr/bin/clang-18)
-set(CMAKE_CXX_COMPILER /usr/bin/clang++-18)
--- .github/workflows/triplets/x64-linux-clang.cmake
+++ .github/workflows/triplets/x64-linux-clang.cmake
@@ -2,4 +2,6 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE .github/workflows/clang-toolchain.cmake)
+
+set(CMAKE_C_COMPILER /usr/bin/clang-18)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++-18)
