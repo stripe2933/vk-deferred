24596d0a488249d42b0f75451c519b5755e59dc3
gomkyung2 <stripe2933@gmail.com>
2024-09-30 18:31:49 +0900

    WIP: fix CI.

--- .github/workflows/scripts/arm64-osx-clang-toolchain.cmake
+++ .github/workflows/scripts/arm64-osx-clang-toolchain.cmake
@@ -1,3 +1,1 @@
 set(CMAKE_CXX_COMPILER /opt/homebrew/opt/llvm@18/bin/clang++)
-set(CMAKE_CXX_FLAGS "-nostdinc++ -nostdlib++ -isystem /opt/homebrew/opt/llvm@18/include/c++/v1")
-set(CMAKE_EXE_LINKER_FLAGS "-L /opt/homebrew/opt/llvm@18/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm@18/lib/c++ -lc++")
